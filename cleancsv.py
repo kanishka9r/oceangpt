0cc14bc812a339abee8bbdf1d7c66d42709baaf5
kanishka9r <kanishkarajput4918@gmail.com>
2025-09-24 13:26:30 +0530

    sql

--- cleancsv.py
+++ cleancsv.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import sqlite3
 
 # Input folder containing CSVs
 input_folder = r"C:\Users\kanis\Downloads\KANISHKA\PROJECT\sih floatchat\data\csv_output"
@@ -27,4 +28,18 @@ for file in os.listdir(input_folder):
             print(f"❌ Failed to process {file}: {e}")
 
 
+all_files = [os.path.join(output_folder, f) for f in os.listdir(output_folder) if f.endswith('.csv')]
+df_list = [pd.read_csv(f) for f in all_files]
+df_combined = pd.concat(df_list, ignore_index=True)
 
+conn = sqlite3.connect(r"C:\Users\kanis\Downloads\KANISHKA\PROJECT\sih floatchat\data\datasql.db")  
+table_name = "ocean_table"  # choose your table name
+df_combined.to_sql(table_name, conn, if_exists='replace', index=False) 
+
+cursor = conn.cursor()
+cursor.execute(f"SELECT * FROM {table_name} LIMIT 5;")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+conn.close()
